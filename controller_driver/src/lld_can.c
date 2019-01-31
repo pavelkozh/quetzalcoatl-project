8b8d3b55e68e0567085d329e8cdee7c867b10367
AndrewKh7 <andrey.i.khrapkov@gmail.com>
2019-01-31 14:01:21 +0400

    Рабочик вариант! CAN1 - шлет и слушает. CAN2 - слушает. ISPDAS - шлет и слушает. CAN1 и CAN2 работают через MCP2551

--- controller_driver/src/lld_can.c
+++ controller_driver/src/lld_can.c
@@ -2,38 +2,52 @@
 
 
 
-static const CANConfig cancfg = {
+static const CANConfig cancfg1 = {
 CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
-CAN_BTR_LBKM | CAN_BTR_SJW(0) | CAN_BTR_TS2(0) |
+ CAN_BTR_SJW(0) | CAN_BTR_TS2(0) |
 CAN_BTR_TS1(5) | CAN_BTR_BRP(26)
 };
-
+static const CANConfig cancfg2 = {
+CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
+ CAN_BTR_SJW(0) | CAN_BTR_TS2(0) |
+CAN_BTR_TS1(5) | CAN_BTR_BRP(26)
+};
 
 
 /*
  * Receiver thread.
  */
 static THD_WORKING_AREA(can_rx1_wa, 256);
+static THD_WORKING_AREA(can_rx2_wa, 256);
 static THD_FUNCTION(can_rx, arg) {
     arg = arg;
 
-    event_listener_t el;
+    event_listener_t el2;
+    event_listener_t el1;
+
     chRegSetThreadName("receiver");
-    chEvtRegister(&CAND1.rxfull_event, &el, 0);
+    chEvtRegister(&CAND2.rxfull_event, &el2, 0);
+    chEvtRegister(&CAND1.rxfull_event, &el1, 0);
 
     while (1)
     {
       if (chEvtWaitAnyTimeout(ALL_EVENTS, MS2ST(100)) == 0)
         continue;
-      while ( canReceive(&CAND1, CAN_ANY_MAILBOX, &rxmsg, TIME_IMMEDIATE) == MSG_OK)
+      while ( canReceive(&CAND2, CAN_ANY_MAILBOX, &rxmsg, TIME_IMMEDIATE) == MSG_OK)
       {
         /* Process message.*/
-        palTogglePad(GPIOB,0);
+        palTogglePad(GPIOB,0); //Зелёный
       }
+      while ( canReceive(&CAND1, CAN_ANY_MAILBOX, &rxmsg, TIME_IMMEDIATE) == MSG_OK)
+            {
+              /* Process message.*/
+              palTogglePad(GPIOB,14); //Красный
+            }
       chThdSleepMilliseconds( 10 );
 
     }
-    chEvtUnregister(&CAND1.rxfull_event, &el);
+    chEvtUnregister(&CAND2.rxfull_event, &el2);
+    chEvtUnregister(&CAND1.rxfull_event, &el1);
 
   }
 
@@ -55,9 +69,9 @@ static THD_FUNCTION(can_tx, p) {
   while (true) {
 
     if( canTransmit(&CAND1, CAN_ANY_MAILBOX, &txmsg, MS2ST(100)) == MSG_OK){
-         palTogglePad(GPIOB,7);
+         palTogglePad(GPIOB,7);//Синий
     }else{
-         palTogglePad(GPIOB,14);
+//         palTogglePad(GPIOB,14); //Крачный
     }
 
     chThdSleepMilliseconds(500);
@@ -68,8 +82,12 @@ void can_init ( void )
 {
     palSetPadMode(GPIOD,1,PAL_MODE_ALTERNATE(9));
     palSetPadMode(GPIOD,0,PAL_MODE_ALTERNATE(9));
-    canStart(&CAND1, &cancfg);
+    palSetPadMode(GPIOB,5,PAL_MODE_ALTERNATE(9));
+    palSetPadMode(GPIOB,6,PAL_MODE_ALTERNATE(9));
+    canStart(&CAND1, &cancfg1);
+    canStart(&CAND2, &cancfg2);
     chThdCreateStatic(can_rx1_wa, sizeof(can_rx1_wa), NORMALPRIO + 7, can_rx, NULL);
+    chThdCreateStatic(can_rx2_wa, sizeof(can_rx2_wa), NORMALPRIO + 7, can_rx, NULL);
     chThdCreateStatic(can_tx_wa, sizeof(can_tx_wa), NORMALPRIO + 6, can_tx, NULL);
 
 }
