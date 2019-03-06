f5f7f920a6aceac418a1b951bfa58f3358e7767f
TatianaEtu <stm1995@mail.ru>
2019-03-06 13:00:19 +0300

    [lld_encoder]: Код обработки экодера из проекта AutoNetChellenge
    Изменено количество импульсов на оборот энкодера

--- controller_driver/src/lld_encoder.c
+++ controller_driver/src/lld_encoder.c
@@ -1,5 +1,6 @@
 #include <lld_encoder.h>
 
+#if 0
 /* Wheel position sensor connects to wheelPosSensorInLine pin*/
 #define encoderInLine       PAL_LINE ( GPIOF, 13 )
 
@@ -38,7 +39,7 @@ static bool         isRotating                      = false;
 
 /* Wheel position sensor configuration
  * impulse quantity per revolution*/
-#define ImpsPerRevQuantity      8
+#define ImpsPerRevQuantity      1000
 
 /* PartOfWheelRevPerMinute stands for part of revolution per minute
  * uses for velocity calculation   */
@@ -195,5 +196,180 @@ Position_t encoderGetPosition ( void )
     return position;
 }
 
+#endif
 
+#include <tests.h>
+#include <lld_encoder.h>
+
+#define ENC_MAX_TICK_NUM        1000
+
+/*******************************/
+/***    LINE CONFIGURATION   ***/
+/*******************************/
+
+#define ENCODER_CH_A_LINE   PAL_LINE( GPIOD, 5 )
+#define ENCODER_CH_B_LINE   PAL_LINE( GPIOD, 4 )
+#define ENCODER_NULL_LINE   PAL_LINE( GPIOD, 3 )
+
+/*******************************/
+
+
+/***********************************/
+/***    Variable CONFIGURATION   ***/
+/***********************************/
+
+rawEncoderValue_t       enc_tick_cntr       = 0;
+rawRevEncoderValue_t    enc_revs_cntr       = 0;
+
+rawEncoderValue_t       enc_null_revs_cntr  = 0;
+
+bool                    enc_dir_state       = 0;
+
+/***********************************/
+
+
+/***    Base channel processing     ***/
+static void extcb_base(EXTDriver *extp, expchannel_t channel)
+{
+    (void)extp;
+    (void)channel;
+
+    /***    To define direction of encoder rotation  ***/
+    if( palReadLine( ENCODER_CH_B_LINE ) == 0 )
+    {
+        enc_tick_cntr    += 1;
+        enc_dir_state    = 1;       // counterclockwise
+    }
+    else
+    {
+        enc_tick_cntr    -= 1;
+        enc_dir_state    = 0;       // clockwise
+    }
+
+    /***    Reset counter when it reaches the MAX value  ***/
+    /***    Count encoder revolutions                    ***/
+    if( enc_tick_cntr == (ENC_MAX_TICK_NUM - 1) )
+    {
+        enc_revs_cntr   += 1;
+        enc_tick_cntr    = 0;
+    }
+    else if( enc_tick_cntr == (-ENC_MAX_TICK_NUM + 1) )
+    {
+        enc_revs_cntr   -= 1;
+        enc_tick_cntr    = 0;
+    }
+
+}
+
+static void extcb_dir(EXTDriver *extp, expchannel_t channel)
+{
+    (void)extp;
+    (void)channel;
+
+}
+
+/***    NULL Point of encoder processing    ***/
+static void extcb_null(EXTDriver *extp, expchannel_t channel)
+{
+    (void)extp;
+    (void)channel;
+
+    if( enc_dir_state == 0 )  enc_null_revs_cntr -= 1;
+    else                      enc_null_revs_cntr += 1;
+
+
+}
+
+/********************************/
+/*** Configuration structures ***/
+/********************************/
+
+
+static const EXTConfig extcfg =
+{
+   {
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_RISING_EDGE | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOD , extcb_null}, // PD3
+    {EXT_CH_MODE_BOTH_EDGES  | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOD , extcb_dir},  // PD4
+    {EXT_CH_MODE_RISING_EDGE | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOD , extcb_base}, // PD5
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL},
+    {EXT_CH_MODE_DISABLED, NULL}
+  }
+};
+
+static bool         isInitialized       = false;
+
+/**
+ * @brief   Initialize periphery connected to encoder
+ * @note    Stable for repeated calls
+ */
+void lldEncoderInit( void )
+{
+    if ( isInitialized )
+            return;
+
+    extStart( &EXTD1, &extcfg );
+
+    /* Set initialization flag */
+    isInitialized = true;
+}
+
+/**
+ * @brief   Get number of encoder ticks
+ * @note    Max number of ticks is defined by MAX_TICK_NUM
+ * @return  Encoder ticks number depends on direction of rotation
+ *          [0; ENC_MAX_TICK_NUM]
+ *          after ENC_MAX_TICK_NUM it resets
+ */
+rawEncoderValue_t lldGetEncoderRawTicks( void )
+{
+    return enc_tick_cntr;
+}
+
+/**
+ * @brief   Get direction of encoder rotation
+ * @return  clockwise           -> 0
+ *          counterclockwise    -> 1
+ */
+bool lldGetEncoderDirection( void )
+{
+    return enc_dir_state;
+}
+
+/**
+ * @brief   Get number of encoder revolutions [double]
+ * @note    1 revolution = MAX_TICK_NUM ticks
+ * @return  Encoder revolutions number depends on direction of rotation
+ */
+rawRevEncoderValue_t   lldGetEncoderRawRevs( void )
+{
+    return ( enc_revs_cntr + enc_tick_cntr / (float)ENC_MAX_TICK_NUM );
+}
+
+/**
+ * @brief   Get number of encoder revolutions [int]
+ * @note    If you use absolute encoder!!!
+ * @return  Encoder revolutions number depends on direction of rotation
+ */
+rawEncoderValue_t   lldGetAbsoluteEncoderRawRevs( void )
+{
+    return enc_null_revs_cntr;
+}
 
