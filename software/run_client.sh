f9e6ead9e17ccdac046d5e1346f8b8b2b67a0cad
Alexey <nine-4lex@mail.ru>
2019-07-11 16:15:30 +0300

    [soft/gstreamer] Fixed scripts for server/client streaming

--- software/run_client.sh
+++ software/run_client.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-echo "GStreamer Logitech C920 Pro HD client"
+# echo "GStreamer Logitech C920 Pro HD client"
 
-echo -n "Provide port to listen (e. g. 6667): "
-read user_port
+# echo -n "Provide port to listen (e. g. 6667): "
+# read user_port
+user_port=5000
 
-echo "Starting stream..."
-gst-launch-1.0 -v udpsrc port=$user_port ! application/x-rtp, encoding-name=H264 ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink
+# echo "Starting stream..."
+gst-launch-1.0 -v udpsrc port=$user_port ! \
+    application/x-rtp, encoding-name=H264 ! \
+    rtph264depay ! avdec_h264 ! \
+    videoconvert ! fpsdisplaysink sync=false
--- software/run_server.sh
+++ software/run_server.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 clear
-echo "GStreamer Logitech C920 Pro HD stream server"
+# echo "GStreamer Logitech C920 Pro HD stream server"
 
-echo -n "Provide device name (e .g. /dev/video0):"
-read dev_name
+# echo -n "Provide device name (e .g. /dev/video0):"
+# read dev_name
 
-echo -n "Provide client IP (e. g. 192.168.1.1):"
-read client_IP
+# echo -n "Provide client IP (e. g. 192.168.1.1):"
+# read client_IP
 
-echo -n "Provide client port (e. g. 6667):"
-read client_port
+# echo -n "Provide client port (e. g. 6667):"
+# read client_port
 
-echo -n "Provide picture width and heigh, space-separated (e. g. 1280 720):"
-read cam_width cam_height
+# echo -n "Provide picture width and heigh, space-separated (e. g. 1280 720):"
+# read cam_width cam_height
+
+dev_name=/dev/video1
+
+client_IP=127.0.0.1
+client_port=5000
+
+cam_width=800
+cam_height=600
+
+
+# gst-launch-1.0 -v videotestsrc ! \
+#     video/x-raw,width=$cam_width,height=$cam_height,framerate=30/1 ! videoconvert ! \
+#     x264enc tune=zerolatency bitrate=2048 speed-preset=superfast ! \
+#     rtph264pay ! udpsink host=$client_IP port=$client_port
+# exit
 
 echo "Starting stream..."
-gst-launch-1.0 -v -e v4l2src device=$dev_name ! queue ! video/x-h264,width=$cam_width,height=$cam_height,framerate=30/1 ! rtph264pay ! udpsink host=$client_IP port=$client_port
-
+gst-launch-1.0 -v -e v4l2src device=$dev_name ! \
+    video/x-h264,width=$cam_width,height=$cam_height,framerate=30/1,bitrate=2048 ! \
+    h264parse ! rtph264pay ! udpsink host=$client_IP port=$client_port
+    # avdec_h264 ! fpsdisplaysink sync=false
