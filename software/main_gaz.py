0c672f8737dd91e70219b4d5053b29eae1402621
kataXV <katastrofistche@gmail.com>
2019-07-11 19:50:13 +0300

    Фикс ПО для передачи данных между пультом и газелью

--- software/main_gaz.py
+++ software/main_gaz.py
@@ -4,30 +4,32 @@ import time
 import command
 # MAIN for gazel computer
 
-def on_msg(data):
-	dictionary = command.CommandParse.parse(data)
-	if dictionary['type'] == command.CommandTypes.COMM_ENABLE:
-		comun.activate_connection()
-		print("COMM_ENABLE")
-	if dictionary['type'] == command.CommandTypes.COMM_DISABLE:
-		comun.deactivate_connection()
-		print("COMM_DISABLE")
-	if dictionary['type'] == command.CommandTypes.COMM_START:
-		comun.on_start()
-		print("COMM_START")
-	if dictionary['type'] == command.CommandTypes.COMM_STOP:
-		comun.on_stop()
-		print("COMM_STOP")
-	if dictionary['type'] == command.CommandTypes.COMM_SET:
-		print("COMM_SET")
-		speed, angle = dictionary['value']
-		comun.set_control(speed, angle)
-		print(speed)
-		print(angle)
-test_work = True
-sub = mc.MQTTControlSub(on_msg=on_msg)
-comun = communication.CommunicationOnSerial('/dev/ttyACM0')
+BROCKER_IP = '127.0.0.1'
+
 print('start')
+comun = communication.CommunicationOnSerial('/dev/ttyACM0')
+sub = command.CommandSub(ip = BROCKER_IP)
+def set_handler(load):
+	comun.set_control(load['speed'], load['steer'],)
+	print('COMM_SET: speed = {}, steer = {}'.format(load['speed'], load['steer']))
+def start_handler():
+	comun.on_start()
+	print("COMM_START")
+def stop_handler():
+	comun.on_stop()
+	print("COMM_STOP")
+def enable_handler():
+	comun.activate_connection()
+	print("COMM_ENABLE")
+def disable_handler():
+	comun.deactivate_connection()
+	print("COMM_DISABLE")
+sub.on_set = set_handler
+sub.on_start = start_handler
+sub.on_stop = stop_handler
+sub.on_enable = enable_handler
+sub.on_disable = disable_handler
+test_work = True
 try:
     while test_work:
         time.sleep(1)
--- software/main_teleop.py
+++ software/main_teleop.py
@@ -4,99 +4,80 @@ from threading import Thread
 import time
 import pygame
 # MAIN for gamepad+computer
-
+BROCKER_IP = '127.0.0.1'
 pygame.init()
 joystick_disabled = False
 hrz_value_delay = 0
 vrt_value_delay = 0
 try:
-    joystick = pygame.joystick.Joystick(0)
-    joystick.init()
-    print("init")
-    joy_vrt_axis = 4
-    joy_hrz_axis = 0
+	joystick = pygame.joystick.Joystick(0)
+	joystick.init()
+	print("init")
+	joy_vrt_axis = 4
+	joy_hrz_axis = 0
 
 except:
-    joystick_disabled = True
-    print('except')
-json_str = None
-def thread_pub():
-    pub = mc.MQTTControlPub()
-    try:
-    	isActive = True
-    	prev_type_ = None
-    	prev_vrt = None
-    	prev_hrz = None
-    	vrt, hrz = 0, 0
-    	type_ = None
-    	value = [None, None]
-    	print("start")
-    	while isActive:
-    		for event in pygame.event.get():
-	            if event.type == pygame.QUIT:
-	                pygame.quit()
-	                isActive = False
-	            if not joystick_disabled:
-	                if event.type == pygame.JOYAXISMOTION:
-	                	type_ = command.CommandTypes.COMM_SET
-	                	if event.axis == joy_hrz_axis:
-	                		if abs(event.value) > 0.02:
-	                			hrz = (int)(event.value * 100)
-	                		else:
-	                			hrz = 0
-	                	elif event.axis == joy_vrt_axis:
-	                		if abs(event.value) > 0.02:
-	                			vrt = -1 * (int)(event.value * 100)
-	                			if vrt == 0:
-	                				vrt = 0
-	                			elif vrt >= 1 and vrt <= 34:
-	                				vrt = 5
-	                			elif vrt >= 35 and vrt <= 65:
-	                				vrt = 10
-	                			elif vrt >= 66 and vrt <= 100:
-	                				vrt = 15
-	                			elif vrt <= -1 and vrt >= -34:
-	                				vrt = -5
-	                			elif vrt <= -35 and vrt >= -65:
-	                				vrt = -10
-	                			elif vrt <= -66 and vrt >= -100:
-	                				vrt = -15
-	                		else:
-	                			vrt = 0
-	                	value = [vrt, hrz]
-	                        
-	                if event.type == pygame.JOYBUTTONDOWN:
-	                    if event.button == 4:
-	                        type_ = command.CommandTypes.COMM_ENABLE
-	                        value = None
-	                    if event.button == 5:
-	                        type_ = command.CommandTypes.COMM_DISABLE
-	                        value = None
-	                    if event.button == 2:
-	                        type_ = command.CommandTypes.COMM_START
-	                        value = None
-	                    if event.button == 1:
-	                        type_ = command.CommandTypes.COMM_STOP
-	                        value = None
-	            if prev_type_ != type_:
-	            	prev_type_ = type_
-	            	json_str = command.CommandCreate.create(type_, value)
-	            	print(json_str)
-	            	pub.send(json_str)
-	            else:
-	            	if type_ == command.CommandTypes.COMM_SET:
-	            		#Если тип SET - проверяем, что хотя бы одно из значений поменялось
-	            		if value[0] != prev_vrt or value[1] != prev_hrz:
-	            			prev_vrt = value[0]
-	            			prev_hrz = value[1]
-	            			json_str = command.CommandCreate.create(type_, value)
-	            			print(json_str)
-	            			pub.send(json_str)
-    finally:
-	    pygame.quit()
-	    print("exit")
-	    isActive = False
+	joystick_disabled = True
+	print('except')
 
-Thread(target=thread_pub).start()
+json_str = None
+pub = command.CommandPub(ip = BROCKER_IP)
+try:
+	isActive = True
+	prev_type_ = None
+	vrt, hrz = 0, 0
+	type_ = None
+	print("start")
+	while isActive:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				isActive = False
+			if not joystick_disabled:
+				if event.type == pygame.JOYAXISMOTION:
+					if event.axis == joy_hrz_axis:
+						if abs(event.value) > 0.02:
+							hrz = (int)(event.value * 100)
+						else:
+							hrz = 0
+					elif event.axis == joy_vrt_axis:
+						if abs(event.value) > 0.02:
+							vrt = -1 * (int)(event.value * 100)
+							if vrt == 0:
+								vrt = 0
+							elif vrt >= 1 and vrt <= 34:
+								vrt = 5
+							elif vrt >= 35 and vrt <= 65:
+								vrt = 10
+							elif vrt >= 66 and vrt <= 100:
+								vrt = 15
+							elif vrt <= -1 and vrt >= -34:
+								vrt = -5
+							elif vrt <= -35 and vrt >= -65:
+								vrt = -10
+							elif vrt <= -66 and vrt >= -100:
+								vrt = -15
+						else:
+							vrt = 0
+					pub.send_set(vrt, hrz)
+					print('send_enable: vrt = {}, hrz = {}'.format(vrt, hrz))
+
+				if event.type == pygame.JOYBUTTONDOWN:
+					if event.button == 4:
+						pub.send_enable()
+						print('send_enable')
+					if event.button == 5:
+						pub.send_disable()
+						print('send_disable')
+					if event.button == 2:
+						pub.send_start()
+						print('send_start')
+					if event.button == 1:
+						pub.send_stop()
+						print('send_stop')
+finally:
+	pygame.quit()
+	print("exit")
+	isActive = False
 
 
